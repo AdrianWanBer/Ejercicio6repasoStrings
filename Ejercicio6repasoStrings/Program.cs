b45bdcca3129f37a7a3adf53919f1cd2251fff5b
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-03-12 14:15:06 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio6repasoStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ejercicio6repasoStrings
+{
+    class program
+    {
+     public static void Main(String[] args)
+        {
+               Console.Write("Ingrese una cadena: ");
+               string entrada = Console.ReadLine();
+                Console.WriteLine(entrada.Reverse().ToArray());
+            
+            }
+        }
+
+}
